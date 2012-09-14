325780eaee5a7544f5c6acdcdac54f21e28e3992
Jim Weirich <jim.weirich@gmail.com>
2012-09-14 17:56:22 -0400

    initial commit

---
+++ spec/fm_spec.rb
@@ -0,0 +1,22 @@
+require 'rspec/given'
+
+RSpec.configure do |config|
+  config.mock_with :flexmock
+end
+
+describe "Using FlexMock under RSpec" do
+
+  When { m.foobar }
+
+  context "when using mocks" do
+    Given(:m) { flexmock("obj") }
+    Given { m.should_receive(:foobar => "HI").once }
+    Then { }
+  end
+
+  context "when using spies" do
+    Given(:m) { flexmock("obj", :foobar => "HI") }
+    Then { m.should have_received(:foobar).once }
+  end
+
+end
